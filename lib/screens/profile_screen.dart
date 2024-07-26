af6fef11dbe186d2737e816dcb111f0831783034
Ritwiz <madangopalsingh2803@gmail.com>
2024-07-26 23:55:08 +0530

    saving the data on cloud firestore and firebase storage

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, sort_child_properties_last, unused_field
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, sort_child_properties_last, unused_field, unused_local_variable
 
 import 'dart:typed_data';
 import 'package:image_picker/image_picker.dart';
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:robosoc/models/component.dart';
 import 'package:robosoc/utilities/image_picker.dart';
 import 'package:robosoc/widgets/issued_commponent_card.dart';
+import 'package:robosoc/utilities/add_data.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -16,11 +17,23 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   Uint8List? _image; 
+  TextEditingController nameController = TextEditingController();
+  TextEditingController roleController = TextEditingController();
+  //selectImage 
    void selectImage() async {
     Uint8List img = await pickImage(ImageSource.gallery);
     setState(() {
       _image = img;
     });
+  }
+  
+  //Saving data 
+  void saveProfile() async{
+
+      String name = nameController.text;
+      String role = roleController.text;
+
+      String resp = await StoreData().saveData(name: name, role: role, file: _image!);
   }
   final List<Component> _issuedComponents = [
     Component(name: "Ardunio", quantity: 5),
